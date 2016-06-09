a5dda4c15105e14ceafac82ef0a2247e10290a95
Chris Heisel <chris@heisel.org>
2016-06-09 17:06:59 -0400

    Add tests for AgileTicket and FlowLog inserting

--- jira_agile_extractor/models.py
+++ jira_agile_extractor/models.py
@@ -1,2 +1,32 @@
+from datetime import datetime
+
+
 class AgileTicket(object):
-    pass
+    def __init__(self, key):
+        self.key = str(key)
+        self._flow_log = FlowLog()
+
+    @property
+    def flow_log(self):
+        return self._flow_log
+
+
+class FlowLog(list):
+    def append(self, value):
+        try:
+            ('entered_at', 'state') in value.keys()
+        except AttributeError:
+            raise TypeError("Flow log items must have a 'entered_at' datetime and a 'state' string. Got: {value}".format(value=value))
+
+        try:
+            datetime.now() - value['entered_at']
+        except TypeError:
+            entered_at = value['entered_at']
+            msgvars = dict(
+                val_type=type(entered_at),
+                val=entered_at,
+            )
+            raise TypeError("Flow log items must have a entered_at datetime. Got: {val_type} / {val}".format(**msgvars))
+
+        value[u'state'] = unicode(value['state'])
+        super(FlowLog, self).append(value)
---
+++ tests/test_models.py
@@ -0,0 +1,77 @@
+import pytest
+
+
+@pytest.fixture
+def klass():
+    from jira_agile_extractor import AgileTicket
+    return AgileTicket
+
+
+@pytest.fixture
+def datetime():
+    from datetime import datetime
+    return datetime
+
+
+@pytest.fixture
+def make_one(klass):
+    def _make_one(*args, **kwargs):
+        kwargs['key'] = kwargs.get('key', "TEST-1")
+        return klass(*args, **kwargs)
+    return _make_one
+
+
+def test_construction(make_one):
+    t = make_one()
+    assert t
+
+
+def test_flow_log_append_happy(make_one, datetime):
+    t = make_one()
+    t.flow_log.append(
+        dict(
+            entered_at=datetime.now(),
+            state="Missouri"
+        )
+    )
+    assert t.flow_log[0]
+
+
+def test_flow_log_append_unicode(make_one, datetime):
+    t = make_one()
+    t.flow_log.append(
+        dict(
+            entered_at=datetime.now(),
+            state="LA",
+        )
+    )
+    assert t.flow_log[0][u'state'] == unicode("LA")
+
+
+def test_flow_log_append_datetime(make_one, datetime):
+    test_dt = datetime.now()
+    t = make_one()
+    t.flow_log.append(
+        dict(
+            entered_at=test_dt,
+            state="OK"
+        )
+    )
+    assert t.flow_log[0][u'entered_at'] == test_dt
+
+
+def test_flow_log_append_unhappy(make_one, datetime):
+    t = make_one()
+    with pytest.raises(TypeError):
+        t.flow_log.append(
+            dict(
+                state="SD",
+                entered_at=str(datetime.now()),
+            )
+        )
+
+
+def test_flow_log_append_unhappy_no_dict(make_one):
+    t = make_one()
+    with pytest.raises(TypeError):
+        t.flow_log.append(['VT', '278461911'])
