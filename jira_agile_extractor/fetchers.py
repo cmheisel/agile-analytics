c915f4282d4d2be386452c83c253a7208396ed73
Chris Heisel <chris@heisel.org>
2016-06-17 15:09:05 -0400

    More better docstrings

--- jira_agile_extractor/fetchers.py
+++ jira_agile_extractor/fetchers.py
@@ -1,3 +1,5 @@
+"""Fetch data from agile sources and return standard AgileTickets."""
+
 from dateutil.parser import parse
 from jira import JIRA
 
@@ -5,14 +7,33 @@ from models import AgileTicket
 
 
 class BaseFetcher(object):
+    """Base class for Fetchers."""
+
     def fetch():
+        """Method invoked to fetch.
+
+        Args:
+            None
+        Returns:
+            None
+        Raises:
+            NotImplementedError
+        """
         raise NotImplementedError
 
 
 def convert_jira_issue(issue):
     """
-    Accepts a jira.Issue object and returns a jira_agile_extractor.AgileTicket
-    :issue: jira.Issue
+    Convert a JIRA issue into a AgileTicket.
+
+    Args:
+        issue: A jira.Issue instance
+
+    Returns:
+        An AgileTicket instance
+
+    Raises:
+        None
     """
     t = AgileTicket(issue.key)
 
@@ -39,30 +60,64 @@ def convert_jira_issue(issue):
 
 
 class JIRAFetcher(BaseFetcher):
+    """Fetch data from JIRA and transform it into AgileTickets.
+
+    Attributes:
+        None
+    """
+
     BASIC_AUTH_KEYS = ['username', 'password']
     OAUTH_KEYS = ['access_token', 'access_token_secret', 'consumer_key', 'key_cert']
 
     def __init__(self, url, auth, filter_id, max_results=999, jira_kwargs=None):
-        self.url = url
-        self.auth = auth
-        self.auth_kwargs = {}
+        """Create JIRAFetcher.
+
+        Args:
+            url (str): Fully qualified URL include http/https scheme to your JIRA instance
+            auth (dict): Dictionary of authentication credentials. Either username/password keys
+                for basic auth OR access_token/access_token_secret/consumer_key/key_cert for OAuth
+            filter_id (int): The JIRA filter ID of results you wish to fetch
+            max_results (Optional[int]): Number of results to fetch, defaults to 999
+            jira_kwargs (Optional[dict]): Additional kwargs passed to the jira.JIRA class
+                at instance creation
+
+        Returns:
+            JIRAFetcher: instance
+
+        Raises:
+            TypeError: When neither Basic nor Oauth keys were provided in the auth dict
+        """
+        self._url = url
+        self._auth = auth
+        self._auth_kwargs = {}
         self._validate_auth()
-        self.filter_id = int(filter_id)
-        self.jira_kwargs = jira_kwargs or {}
-        self.jira_kwargs.update(self.auth_kwargs)
-        self.max_results = max_results
+        self._filter_id = int(filter_id)
+        self._jira_kwargs = jira_kwargs or {}
+        self._jira_kwargs.update(self.auth_kwargs)
+        self._max_results = max_results
 
     def _validate_auth(self):
-        if set(self.auth.keys()) <= set(self.BASIC_AUTH_KEYS) and set(self.auth.keys()) <= set(self.OAUTH_KEYS):
+        if set(self._auth.keys()) <= set(self.BASIC_AUTH_KEYS) and set(self._auth.keys()) <= set(self.OAUTH_KEYS):
             raise TypeError("Neither %s nor %s found in auth parameter" % (self.BASIC_AUTH_KEYS, self.OAUTH_KEYS))
-        if set(self.auth.keys()) <= set(self.BASIC_AUTH_KEYS):
-            self.auth_kwargs = dict(basic_auth=(self.auth['username'], self.auth['password']))
-        elif set(self.auth.keys()) <= set(self.OAUTH_KEYS):
-            self.auth_kwargs = dict(oauth=self.auth)
+        if set(self._auth.keys()) <= set(self.BASIC_AUTH_KEYS):
+            self._auth_kwargs = dict(basic_auth=(self._auth['username'], self._auth['password']))
+        elif set(self._auth.keys()) <= set(self.OAUTH_KEYS):
+            self._auth_kwargs = dict(oauth=self.auth)
         else:
             raise TypeError("Neither %s nor %s found in auth parameter" % (self.BASIC_AUTH_KEYS, self.OAUTH_KEYS))
 
     def fetch(self, jira_klass=JIRA):
+        """Fetch data and return AgileTickets.
+
+        Args:
+            jira_klass (Optional[JIRA]): jira.JIRA compatible class to be used for a JIRA connection
+
+        Returns:
+            list: List of AgileTicket instances or empty list
+
+        Raises:
+            None
+        """
         j = jira_klass(
             server=self.url,
             **self.jira_kwargs
