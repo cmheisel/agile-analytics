bb7c9f45529731836cac7c70e1e174bf3b92e0fe
Chris Heisel <chris@heisel.org>
2016-07-31 14:35:30 -0400

    Fix version finding/packaging

--- setup.py
+++ setup.py
@@ -3,8 +3,6 @@
 from __future__ import unicode_literals
 from setuptools import setup, find_packages
 
-import agile_analytics
-
 from pip.req import parse_requirements
 
 # parse_requirements() returns generator of pip.req.InstallRequirement objects
@@ -14,10 +12,13 @@ install_reqs = parse_requirements("requirements.txt", session=False)
 # e.g. ['django==1.5.1', 'mezzanine==1.4.6']
 reqs = [str(ir.req) for ir in install_reqs]
 
+description = """Pulls data from agile systems and analyzes it."""
+author = "cmheisel"
 README = open('README.md', 'r').read()
-version = ".".join(map(str, agile_analytics.__version__))
-author = agile_analytics.__author__
-description = agile_analytics.__doc__
+try:
+    version = open('version.txt', 'r').read().strip()
+except FileNotFoundError:
+    version = "unknown"
 
 setup(
     name='agile-analytics',
